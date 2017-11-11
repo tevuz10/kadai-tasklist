a1847fd2703ef4213b54fae6ffaa53ecebb2172e
Tatsuya <zentaly@gmail.com>
2017-11-11 13:29:18 +0000

    kadai submit

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+#  validate :content, presence; true, length: { maximum: 255}
+#  validate :status, presence; true, length: { maximum: 255}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111112709) do
+ActiveRecord::Schema.define(version: 20171111114306) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "content"
   end
 
 end
