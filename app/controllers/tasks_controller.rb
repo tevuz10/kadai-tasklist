2002b461788793c3d77a2c302594f7112cbe6708
Tatsuya <zentaly@gmail.com>
2017-11-20 06:19:01 +0000

    kadai login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,56 +1,51 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
-  def index
-    @tasks = Task.all
-  end
-
-  def show
-  end
-  
-  def new
-    @task = Task.new
-  end
-
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'タスク作成が完了しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash[:danger] = 'タスクの作成に失敗しました。'
+      render 'toppages/index'
     end
   end
   
   def edit
+    @task = current_user.tasks.find(params[:id])
   end
   
   def update
-    
+    @task = current_user.tasks.find(params[:id])
+
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      flash[:success] = 'タスクは正常に更新されました'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'タスクは更新されませんでした'
       render :edit
     end
   end
-  
+
   def destroy
-    @task.destroy
-    
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    @tasks.destroy
+    flash[:success] = 'タスクを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
-end
-
-# Strong Parameter
-def set_task
-  @task = Task.find(params[:id])
-end
-
-def task_params
-   params.require(:task).permit(:content, :status)
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @tasks = current_user.tasks.find_by(id: params[:id])
+    unless @tasks
+      redirect_to root_url
+    end
+  end
 end
--- db/migrate/20171111114306_add_content_to_task.rb
+++
@@ -1,5 +0,0 @@
-class AddContentToTask < ActiveRecord::Migration[5.0]
-  def change
-    add_column :tasks, :content, :string
-  end
-end
